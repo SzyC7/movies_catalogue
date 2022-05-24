5b003330774a36d9f0161087b06a260fd4c3e610
SzyC7 <101209123+SzyC7@users.noreply.github.com>
2022-05-24 19:10:17 +0200

    Update tmdb_client.py

--- tmdb_client.py
+++ tmdb_client.py
@@ -3,6 +3,21 @@ import requests
 import random
 from random import shuffle
 
+
+#Ta funkcja jest tu umieszczona na potrzeby zadania z modulu 14.3
+def call_tmdb_api(endpoint):
+   full_url = f"https://api.themoviedb.org/3/{endpoint}"
+   api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTc2NzIyMjA5ZWJjYTU2MGNiZmU2MDJlMmI1ZTc2MSIsInN1YiI6IjYyN2Y5ZjYyYzkyYzVkMDA2OGFlYmEwNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mabgzv7Z5fThTIfaH9Ww45ilYmMRcsrTLKNXAc1nrNk"
+   headers = {
+       "Authorization": f"Bearer {api_token}"
+   }
+   response = requests.get(full_url, headers=headers)
+   response.raise_for_status()
+   return response.json()
+#koniec funkcji umieszczonej na potrzeby zadania z modulu 14.3
+
+
+
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
     api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTc2NzIyMjA5ZWJjYTU2MGNiZmU2MDJlMmI1ZTc2MSIsInN1YiI6IjYyN2Y5ZjYyYzkyYzVkMDA2OGFlYmEwNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mabgzv7Z5fThTIfaH9Ww45ilYmMRcsrTLKNXAc1nrNk"
@@ -12,9 +27,13 @@ def get_popular_movies():
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
+
+
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
+
+
 
 def get_movies(how_many, list_type):
     #data = get_popular_movies()
@@ -22,6 +41,8 @@ def get_movies(how_many, list_type):
     movies = data["results"]
     random.shuffle(movies)
     return movies[:how_many]
+
+
 
 def get_single_movie(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
@@ -32,6 +53,8 @@ def get_single_movie(movie_id):
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
+
+
 def get_single_movie_cast(movie_id, number):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
     api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTc2NzIyMjA5ZWJjYTU2MGNiZmU2MDJlMmI1ZTc2MSIsInN1YiI6IjYyN2Y5ZjYyYzkyYzVkMDA2OGFlYmEwNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mabgzv7Z5fThTIfaH9Ww45ilYmMRcsrTLKNXAc1nrNk"
@@ -40,6 +63,19 @@ def get_single_movie_cast(movie_id, number):
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()["cast"][:number]
+
+
+
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTc2NzIyMjA5ZWJjYTU2MGNiZmU2MDJlMmI1ZTc2MSIsInN1YiI6IjYyN2Y5ZjYyYzkyYzVkMDA2OGFlYmEwNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mabgzv7Z5fThTIfaH9Ww45ilYmMRcsrTLKNXAc1nrNk"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
 
 def get_movies_list(list_type):
     endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
@@ -50,3 +86,11 @@ def get_movies_list(list_type):
     response = requests.get(endpoint, headers=headers)
     response.raise_for_status()
     return response.json()
+
+
+#Ta funkcja jest tu umieszczona na potrzeby zadania z modulu 14.3
+
+def get_movies_list(list_type):
+   return call_tmdb_api(f"movie/{list_type}")
+
+#koniec funkcji umieszczonej na potrzeby zadania z modulu 14.3
